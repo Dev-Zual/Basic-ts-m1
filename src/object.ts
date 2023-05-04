1411337ed4e9428a52b160b55b3faca8b918a587
Zual Rana <freelancerzual@gmail.com>
2023-05-04 11:46:10 +0600

    literal tye in ts

--- src/object.ts
+++ src/object.ts
@@ -1,9 +1,12 @@
 let users: {
+  //literal type
+  company: "programming hero";
   name: string;
   age: number;
   isMarried: boolean;
   wife?: string;
 } = {
+  company: "programming hero",
   name: "zual",
   age: 25,
   isMarried: false,
