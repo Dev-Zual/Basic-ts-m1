1a54999ec493c7f36ab6d10fe7bc8a08f28154ea
Zual Rana <freelancerzual@gmail.com>
2023-05-05 14:31:02 +0600

    type aliases

---
+++ src/alias.ts
@@ -0,0 +1,13 @@
+type CrushTypes = {
+  name: string;
+  age: number;
+  profession: string;
+  address: string;
+};
+
+const crush1: CrushTypes = {
+  name: "moina pakhi",
+  age: 26,
+  profession: "jobs",
+  address: "serpur",
+};
