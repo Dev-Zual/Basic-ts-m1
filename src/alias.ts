961d7ab43b9e22a217d399c01dd46e4084f9a083
Zual Rana <freelancerzual@gmail.com>
2023-05-05 14:44:57 +0600

    function operation type

--- src/alias.ts
+++ src/alias.ts
@@ -11,3 +11,17 @@ const crush1: CrushTypes = {
   profession: "jobs",
   address: "serpur",
 };
+
+type OperationType = (x: number, y: number) => number;
+
+const calculate = (
+  number1: number,
+  number2: number,
+  operation: OperationType
+) => {
+  return operation(number1, number2);
+};
+
+console.log(calculate(10, 20, (x, y) => x + y));
+console.log(calculate(10, 20, (x, y) => x * y));
+console.log(calculate(10, 20, (x, y) => x / y));
