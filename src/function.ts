b63e6d32f98c8ff528b640bd910d470a235bcafe
Zual Rana <freelancerzual@gmail.com>
2023-05-05 14:08:51 +0600

    ts function

--- src/function.ts
+++ src/function.ts
@@ -1,7 +1,8 @@
-function add(num1: number, num2: number): number {
+// function default parameter
+function add(num1: number, num2: number = 10): number {
   return num1 + num2;
 }
-
+add(20);
 const addArrow = (num1: number, num2: number): number => num1 + num2;
 
 const person: {
@@ -15,3 +16,8 @@ const person: {
     console.log(this.balance + money);
   },
 };
+
+const greetFriends = (fr1: string, fr2: string, fr3: string): void =>
+  console.log(`hi ${fr1}\n hi ${fr2}\n hi ${fr3}`);
+
+greetFriends("kashem", "lasem", "josim");
