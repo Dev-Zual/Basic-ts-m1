484c304f25e194a11ffeb8748b6a9a2e9703defe
Zual Rana <freelancerzual@gmail.com>
2023-05-06 08:51:50 +0600

    intersection

--- src/union-intersection.ts
+++ src/union-intersection.ts
@@ -2,7 +2,14 @@ type NoobDeveloper = {
   name: string;
 };
 
-type JuniorDeveloper = {
+// type JuniorDeveloper = {
+//   name: string;
+//   expertise: string;
+//   experience: number;
+// };
+
+//intersection
+type JuniorDeveloper = NoobDeveloper & {
   name: string;
   expertise: string;
   experience: number;
