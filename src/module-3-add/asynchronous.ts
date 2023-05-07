7be0da29cae4453f0c2115590509fc8ed9fb7439
Zual Rana <freelancerzual@gmail.com>
2023-05-07 16:33:13 +0600

    asynchronous boolean  promise

--- src/module-3-add/asynchronous.ts
+++ src/module-3-add/asynchronous.ts
@@ -12,3 +12,18 @@ const getData = async (): Promise<string> => {
   const data = await makePromise();
   return data;
 };
+
+// boolean promise
+const makeBooleanPromise = (): Promise<boolean> => {
+  return new Promise<boolean>((resolve, reject) => {
+    const data: boolean = true;
+    if (data) {
+      resolve(data);
+    } else reject("failed to fetched data!");
+  });
+};
+
+const getData2 = async (): Promise<boolean> => {
+  const data = await makeBooleanPromise();
+  return data;
+};
