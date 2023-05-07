ab20f1275c8b0ad99e551cc3b8924c88b68bdc99
Zual Rana <freelancerzual@gmail.com>
2023-05-07 15:13:46 +0600

    multipe generic in function

--- src/module-3-add/generic-function.ts
+++ src/module-3-add/generic-function.ts
@@ -6,7 +6,16 @@ const createArray = <T>(param: T): T[] => {
 
 createArray<string>("zual");
 createArray<number>(232);
+
 interface NameInterFace {
   name: string;
 }
 createArray<NameInterFace>({ name: "bangladesh" });
+
+// generic function tuple
+const createArray1 = <X, Y>(param: X, param2: Y): [X, Y] => {
+  return [param, param2];
+};
+
+createArray1<string, string>("zual", "i love my country");
+createArray1<number, Array<string>>(232, ["USA"]);
