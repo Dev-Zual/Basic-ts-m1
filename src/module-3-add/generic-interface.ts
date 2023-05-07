ce01faeef3382deaab124862d9746d5a1c103140
Zual Rana <freelancerzual@gmail.com>
2023-05-07 14:42:32 +0600

    generic interface

---
+++ src/module-3-add/generic-interface.ts
@@ -0,0 +1,51 @@
+// generic interface
+interface CrushInterface<T> {
+  name: string;
+  husband: T;
+}
+const crush: CrushInterface<boolean> = {
+  name: "kate winslet",
+  husband: true,
+};
+const crush2: CrushInterface<string> = {
+  name: "kate",
+  husband: "yes",
+};
+
+// generic type
+type GenericArray<T> = Array<T>;
+const rollNumbers1: GenericArray<number> = [1, 2, 3, 4, 5];
+const rollNumbers2: GenericArray<string> = ["1", "2", "3"];
+const rollNumbers3: GenericArray<boolean> = [true, false];
+// array of object generic
+type NameRollType = {
+  name: string;
+  roll: number;
+};
+const userRollAndName: GenericArray<NameRollType> = [
+  {
+    name: "mr. y",
+    roll: 1,
+  },
+  {
+    name: "mr. z",
+    roll: 2,
+  },
+];
+
+// tuple generic
+type GenericTuple<x, y> = [x, y];
+const relation: GenericTuple<string, string> = ["zual", "na"];
+
+interface IRelationWithSalary {
+  name: string;
+  salary: number;
+}
+
+const relationWithSalary: GenericTuple<IRelationWithSalary, string> = [
+  {
+    name: "zual",
+    salary: 10000000,
+  },
+  "mrs. y",
+];
--- src/module-3-add/generic-type.ts
+++ src/module-3-add/generic-type.ts
@@ -1,22 +1,37 @@
-// generic type
-type GenericArray<T> = Array<T>;
+// // generic type
+// type GenericArray<T> = Array<T>;
+// const rollNumbers1: GenericArray<number> = [1, 2, 3, 4, 5];
+// const rollNumbers2: GenericArray<string> = ["1", "2", "3"];
+// const rollNumbers3: GenericArray<boolean> = [true, false];
+// // array of object generic
+// type NameRollType = {
+//   name: string;
+//   roll: number;
+// };
+// const userRollAndName: GenericArray<NameRollType> = [
+//   {
+//     name: "mr. y",
+//     roll: 1,
+//   },
+//   {
+//     name: "mr. z",
+//     roll: 2,
+//   },
+// ];
 
-const rollNumbers1: GenericArray<number> = [1, 2, 3, 4, 5];
-const rollNumbers2: GenericArray<string> = ["1", "2", "3"];
-const rollNumbers3: GenericArray<boolean> = [true, false];
+// // tuple generic
+// type GenericTuple<x, y> = [x, y];
+// const relation: GenericTuple<string, string> = ["zual", "na"];
 
-type NameRollType = {
-  name: string;
-  roll: number;
-};
+// interface IRelationWithSalary {
+//   name: string;
+//   salary: number;
+// }
 
-const userRollAndName: GenericArray<NameRollType> = [
-  {
-    name: "mr. y",
-    roll: 1,
-  },
-  {
-    name: "mr. z",
-    roll: 2,
-  },
-];
+// const relationWithSalary: GenericTuple<IRelationWithSalary, string> = [
+//   {
+//     name: "zual",
+//     salary: 10000000,
+//   },
+//   "mrs. y",
+// ];
