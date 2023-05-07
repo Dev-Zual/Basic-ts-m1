a3b57d458b3c55534adafe4aed4e87eef364db05
Zual Rana <freelancerzual@gmail.com>
2023-05-07 15:47:32 +0600

    generic constraints

---
+++ src/module-3-add/generic-constraints.ts
@@ -0,0 +1,31 @@
+// generic in spreed operator
+
+interface MandatoryInterface {
+  name: string;
+  age: number;
+  salary: number;
+}
+
+const addMeInMyCrushMind = <T extends MandatoryInterface>(myInfo: T) => {
+  const myCrush = "kate winslet";
+  const newData = { ...myInfo, myCrush };
+  return newData;
+};
+
+type MyInfoType = {
+  name: string;
+  age: number;
+  salary: number;
+  other1: string;
+  other2: string;
+};
+
+const myInfo: MyInfoType = {
+  name: "zual",
+  age: 25,
+  salary: 100000000,
+  other1: "other1",
+  other2: "other2",
+};
+
+const result = addMeInMyCrushMind(myInfo);
--- src/module-3-add/generic-function.ts
+++ src/module-3-add/generic-function.ts
@@ -1,43 +1,43 @@
-//generic function
+// //generic function
 
-const createArray = <T>(param: T): T[] => {
-  return [param];
-};
+// const createArray = <T>(param: T): T[] => {
+//   return [param];
+// };
 
-createArray<string>("zual");
-createArray<number>(232);
+// createArray<string>("zual");
+// createArray<number>(232);
 
-interface NameInterFace {
-  name: string;
-}
-createArray<NameInterFace>({ name: "bangladesh" });
+// interface NameInterFace {
+//   name: string;
+// }
+// createArray<NameInterFace>({ name: "bangladesh" });
 
-// generic function tuple
-const createArray1 = <X, Y>(param: X, param2: Y): [X, Y] => {
-  return [param, param2];
-};
-createArray1<string, string>("zual", "i love my country");
-createArray1<number, Array<string>>(232, ["USA"]);
+// // generic function tuple
+// const createArray1 = <X, Y>(param: X, param2: Y): [X, Y] => {
+//   return [param, param2];
+// };
+// createArray1<string, string>("zual", "i love my country");
+// createArray1<number, Array<string>>(232, ["USA"]);
 
-// generic in spreed operator
+// // generic in spreed operator
 
-const addMeInMyCrushMind = <T>(myInfo: T) => {
-  const myCrush = "kate winslet";
-  const newData = { ...myInfo, myCrush };
-  return newData;
-};
+// const addMeInMyCrushMind = <T>(myInfo: T) => {
+//   const myCrush = "kate winslet";
+//   const newData = { ...myInfo, myCrush };
+//   return newData;
+// };
 
-const myInfo = {
-  name: "zual",
-  age: 25,
-  salary: 100000000,
-};
+// const myInfo = {
+//   name: "zual",
+//   age: 25,
+//   salary: 100000000,
+// };
 
-const result = addMeInMyCrushMind(myInfo);
+// const result = addMeInMyCrushMind(myInfo);
 
-// normal function generic
-function createArray3<X, Y>(param: X, param2: Y): [X, Y] {
-  return [param, param2];
-}
-createArray3<string, string>("zual", "nasima");
-createArray3<string, Array<number>>("zual", [123]);
+// // normal function generic
+// function createArray3<X, Y>(param: X, param2: Y): [X, Y] {
+//   return [param, param2];
+// }
+// createArray3<string, string>("zual", "nasima");
+// createArray3<string, Array<number>>("zual", [123]);
