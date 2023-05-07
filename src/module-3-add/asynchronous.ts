abb12da7b8017efae4cad54050e58be6481e92c8
Zual Rana <freelancerzual@gmail.com>
2023-05-07 16:38:52 +0600

    asynchronous object  promise

--- src/module-3-add/asynchronous.ts
+++ src/module-3-add/asynchronous.ts
@@ -27,3 +27,22 @@ const getData2 = async (): Promise<boolean> => {
   const data = await makeBooleanPromise();
   return data;
 };
+
+interface DataType {
+  data: string;
+}
+
+// object promise
+const makeObjectPromise = (): Promise<DataType> => {
+  return new Promise<DataType>((resolve, reject) => {
+    const data: DataType = { data: "data is fetched" };
+    if (data) {
+      resolve(data);
+    } else reject("failed to fetched data!");
+  });
+};
+
+const getData3 = async (): Promise<DataType> => {
+  const data = await makeObjectPromise();
+  return data;
+};
