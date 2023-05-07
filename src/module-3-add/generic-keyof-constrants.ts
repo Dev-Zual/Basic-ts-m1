f987d646345a4eb394359e9d4bd0db455a7efabd
Zual Rana <freelancerzual@gmail.com>
2023-05-07 16:17:16 +0600

    generic keyof  constraints

---
+++ src/module-3-add/generic-keyof-constrants.ts
@@ -0,0 +1,18 @@
+type PersonType = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+type NewType = "name" | "age" | "address"; // manually
+
+type NewTypeKeyOf = keyof PersonType;
+
+// const a: NewType = 'address'
+// const b: NewTypeKeyOf = 'name'
+
+// generic keyof function
+function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+  obj[key];
+}
+const property = getProperty({ name: "zual", age: 25 }, "age");
