aadc0572dfd6b876a4e3f9267f468c6e41bac590
Zual Rana <freelancerzual@gmail.com>
2023-05-08 09:05:42 +0600

    conditional & nested type

--- src/module-3-add/asynchronous.ts
+++ src/module-3-add/asynchronous.ts
@@ -1,67 +1,67 @@
-//json placeholder fetching
-interface IToDo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-const getToDo = async (): Promise<IToDo> => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  return await res.json();
-};
+// //json placeholder fetching
+// interface IToDo {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
+// const getToDo = async (): Promise<IToDo> => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   return await res.json();
+// };
 
-const getToDoData = async (): Promise<void> => {
-  const result = await getToDo();
-  console.log(result);
-};
+// const getToDoData = async (): Promise<void> => {
+//   const result = await getToDo();
+//   console.log(result);
+// };
 
-getToDoData();
+// getToDoData();
 
-// mocking
-const makePromise = (): Promise<string> => {
-  return new Promise<string>((resolve, reject) => {
-    const data: string = "Data is fetched";
-    if (data) {
-      resolve(data);
-    } else reject("failed to fetched data!");
-  });
-};
+// // mocking
+// const makePromise = (): Promise<string> => {
+//   return new Promise<string>((resolve, reject) => {
+//     const data: string = "Data is fetched";
+//     if (data) {
+//       resolve(data);
+//     } else reject("failed to fetched data!");
+//   });
+// };
 
-const getData = async (): Promise<string> => {
-  const data = await makePromise();
-  return data;
-};
+// const getData = async (): Promise<string> => {
+//   const data = await makePromise();
+//   return data;
+// };
 
-// boolean promise
-const makeBooleanPromise = (): Promise<boolean> => {
-  return new Promise<boolean>((resolve, reject) => {
-    const data: boolean = true;
-    if (data) {
-      resolve(data);
-    } else reject("failed to fetched data!");
-  });
-};
+// // boolean promise
+// const makeBooleanPromise = (): Promise<boolean> => {
+//   return new Promise<boolean>((resolve, reject) => {
+//     const data: boolean = true;
+//     if (data) {
+//       resolve(data);
+//     } else reject("failed to fetched data!");
+//   });
+// };
 
-const getData2 = async (): Promise<boolean> => {
-  const data = await makeBooleanPromise();
-  return data;
-};
+// const getData2 = async (): Promise<boolean> => {
+//   const data = await makeBooleanPromise();
+//   return data;
+// };
 
-interface DataType {
-  data: string;
-}
+// interface DataType {
+//   data: string;
+// }
 
-// object promise
-const makeObjectPromise = (): Promise<DataType> => {
-  return new Promise<DataType>((resolve, reject) => {
-    const data: DataType = { data: "data is fetched" };
-    if (data) {
-      resolve(data);
-    } else reject("failed to fetched data!");
-  });
-};
+// // object promise
+// const makeObjectPromise = (): Promise<DataType> => {
+//   return new Promise<DataType>((resolve, reject) => {
+//     const data: DataType = { data: "data is fetched" };
+//     if (data) {
+//       resolve(data);
+//     } else reject("failed to fetched data!");
+//   });
+// };
 
-const getData3 = async (): Promise<DataType> => {
-  const data = await makeObjectPromise();
-  return data;
-};
+// const getData3 = async (): Promise<DataType> => {
+//   const data = await makeObjectPromise();
+//   return data;
+// };
---
+++ src/module-3-add/conditional-types.ts
@@ -0,0 +1,24 @@
+// If a type is dependent on another type, it is called a conditional type.
+
+type a1 = null;
+type a3 = undefined;
+type a4 = number;
+type a2 = a1 extends string ? string : null;
+
+// nested conditional type
+type d = a1 extends number
+  ? number
+  : a3 extends number
+  ? number
+  : a4 extends null
+  ? null
+  : never;
+
+type Sheikh = {
+  wife1: string;
+  wife2: string;
+};
+
+type CheckProperty<X> = X extends keyof Sheikh ? true : false;
+
+type CheckWife1 = CheckProperty<"wife2">;
