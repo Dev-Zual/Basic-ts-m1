92c0488449e21093a8898b7ae3620e58dd0da9a7
Zual Rana <freelancerzual@gmail.com>
2023-05-08 12:55:59 +0600

    key of guard

--- src/module-4/inheritence.ts
+++ src/module-4/inheritence.ts
@@ -1,4 +1,4 @@
-class Person {
+/* class Person {
   name: string;
   age: number;
   address: string;
@@ -41,3 +41,4 @@ class Teacher extends Person {
 
 const teacher1 = new Teacher("mr.kashem", 50, "uganda", "teacher");
 teacher1.takeClass(4);
+ */
