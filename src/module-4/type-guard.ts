e0f51e4c92c27cb6214bf6917b589a2b6cfa1897
Zual Rana <freelancerzual@gmail.com>
2023-05-13 09:50:14 +0600

    instance guard

--- src/module-4/type-guard.ts
+++ src/module-4/type-guard.ts
@@ -18,7 +18,7 @@ type AdminUser = {
   role: "admin";
 };
 
-function getUser(user: NormalUserType | AdminUser) {
+function getUser(user: NormalUserType | AdminUser): string {
   if ("role" in user) {
     return `hello i'm an ${user.role}`;
   } else {
@@ -31,3 +31,57 @@ const adminUser1 = { name: "Mr. Admin", role: "admin" };
 
 console.log(getUser(normalUser1));
 console.log(getUser(adminUser1));
+
+// instanceof guard
+
+class Animal {
+  name: string;
+  species: string;
+  constructor(name: string, species: string) {
+    this.name = name;
+    this.species = species;
+  }
+
+  makeSound(): void {
+    console.log("im making sound");
+  }
+}
+
+class Dog extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeBark(): void {
+    console.log("im barking");
+  }
+}
+class Cat extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeMew(): void {
+    console.log("im mew");
+  }
+}
+
+function isDog(animal: Animal): animal is Dog {
+  return animal instanceof Dog;
+}
+function isCat(animal: Animal): animal is Cat {
+  return animal instanceof Cat;
+}
+
+function getAnimal(animal: Animal) {
+  if (isDog(animal)) {
+    animal.makeBark();
+  } else if (isCat(animal)) {
+    animal.makeMew();
+  } else {
+    animal.makeSound();
+  }
+}
+
+const animal1 = new Dog("german", "dog1"); // instance -> dog
+const animal2 = new Cat("deshi", "cat"); // instance -> cat
+
+getAnimal(animal2);
