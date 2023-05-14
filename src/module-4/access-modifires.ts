978375eee06d69177b2f11c0aeb1a5b36e9713a5
Zual Rana <freelancerzual@gmail.com>
2023-05-14 10:59:44 +0600

    getter & setter

--- src/module-4/access-modifires.ts
+++ src/module-4/access-modifires.ts
@@ -1,14 +1,15 @@
-class BankAccount {
-  id: number;
-  name: string;
-  balance: number;
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this.balance = balance;
-  }
-}
+// class BankAccount {
+//   id: number;
+//   name: string;
+//  private _balance: number;
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
+// }
 
-const myAccount = new BankAccount(111, "zual", 222);
-myAccount.balance = 0;
-console.log(myAccount);
+// const myAccount = new BankAccount(111, "zual", 222);
+// myAccount.
+// myAccount.balance = 0;
+// console.log(myAccount);
---
+++ src/module-4/getter-setter.ts
@@ -0,0 +1,23 @@
+class BankAccount {
+  id: number;
+  name: string;
+  private _balance: number;
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+  // getter
+  get balance(): number {
+    return this._balance;
+  }
+  //setter
+  set setBalance(amount: number) {
+    this._balance = this._balance + amount;
+  }
+}
+
+const myAccount = new BankAccount(111, "zual", 222);
+
+console.log((myAccount.setBalance = 333));
+console.log(myAccount.balance);
