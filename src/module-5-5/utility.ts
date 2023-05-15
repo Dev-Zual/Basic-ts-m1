912065fcb9c6f5bddfff8a98def824d659e8fa76
Zual Rana <freelancerzual@gmail.com>
2023-05-15 15:25:32 +0600

    Partial Required Readonly type

--- src/module-5-5/utility.ts
+++ src/module-5-5/utility.ts
@@ -12,3 +12,11 @@ type Contact = Pick<Person, "contactNo" | "email">;
 
 // Omit type
 type Names = Omit<Person, "email" | "contactNo">;
+
+// partial
+// to make all the properties be optional type
+type Optional = Partial<Person>;
+
+// Required
+// to make all the properties be required
+type RequiredType = Required<Person>;
